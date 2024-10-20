bb80c988ee343e36fc7d2f538cfec0fde259f159
Kiersten Brennan <kiersten.brennan@vanderbilt.edu>
2024-10-20 17:06:57 -0500

    initial commit

---
+++ app/src/pages/CreateWorkshop.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import { Formik, Form, Field, FieldArray } from "formik"
+import Navbar from "../components/Navbar"
+import * as Yup from "yup"
+
+const CreateWorkshop = () => {
+    const initialValues = {
+        name: "",
+        description: "",
+    }
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        description: Yup.string().required("Description is required"),
+    })
+    const handleSubmit = (values: any) => {
+        console.log(values)
+    }
+    return (
+      <>
+        <Navbar />
+        <h1>Create Workshop</h1>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            {({ values, errors, touched, isSubmitting }) => (
+                <Form>
+                    <div className="Form-group">
+                        <label htmlFor="name">Name</label>
+                        <Field 
+                            type="text" 
+                            name="name" 
+                            placeholder="Name" 
+                            className="Form-input-box"
+                        />
+                    </div>
+                    <div className="Form-group">
+                        <label htmlFor="description">Description</label>
+                        <Field 
+                            type="text" 
+                            name="description" 
+                            placeholder="Description" 
+                            className="Form-input-box"
+                        />
+                    </div>
+                    <button 
+                        type="submit" 
+                        className="Button Button-color--dark-1000 Width--100"
+                    >
+                        Create Workshop
+                    </button>
+                </Form>
+            )}
+        </Formik>
+      </>
+    )
+  }
+  
+  export default CreateWorkshop
