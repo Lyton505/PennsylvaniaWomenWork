79589afb2bc83cb2d2cdf53c481eb40087b943b5
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-10-24 15:03:59 -0500

    adding backend logic

---
+++ api/src/routes/index.ts
@@ -0,0 +1,40 @@
+import user from "./user"
+
+export { user }
+
+// import express, { Request, Response, Application } from "express"
+// import cors from "cors"
+// import dbConnect from "./config/dbConnect"
+// import mongoose from "mongoose"
+
+// import { Group, IGroup } from "./model/Group"
+// import { User, IUser } from "./model/User"
+// import { Expense } from "./model/Expense"
+// import { populate } from "dotenv"
+
+// dbConnect()
+
+// const app: Application = express()
+// app.use(cors())
+// app.use(express.json()) // To parse JSON bodies
+// const port = process.env.PORT || 8000
+
+// // Test POST endpoint
+// app.post("/test", (req: Request, res: Response) => {
+//   console.log(req.body) // Log the request body to check what you are receiving
+
+//   const { key } = req.body // Extract "key" from the request body
+//   if (!key) {
+//     return res.status(400).json({ message: "No key provided" })
+//   }
+
+//   return res.status(200).json({
+//     message: "Data received successfully",
+//     receivedKey: key,
+//   })
+// })
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`)
+// })
