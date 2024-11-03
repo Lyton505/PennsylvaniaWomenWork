e51654be89db8233d437cdba59c03e157b398e96
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2024-11-03 15:05:41 -0600

    adding comments

--- api/src/model/Workshop.ts
+++ api/src/model/Workshop.ts
@@ -1,30 +1,30 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import { IUser } from './User';
-
+import mongoose, { Schema, Document } from "mongoose"
 // interface
 interface IWorkshop extends Document {
-    mentor: Schema.Types.ObjectId | IUser;
-    mentee: Schema.Types.ObjectId | IUser;
-    textContent: string;
-    createdAt: Date;
+  mentor: Schema.Types.ObjectId // TODO: add mentor type
+  mentee: Schema.Types.ObjectId // TODO: add mentee type
+  textContent: string
+  createdAt: Date
 
-    updateContent(newContent: string): Promise<void>;
+  updateContent(newContent: string): Promise<void>
 }
 
 // workshop schema
 const WorkshopSchema: Schema<IWorkshop> = new Schema({
-    mentor: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    mentee: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    textContent: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+  mentor: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  mentee: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  textContent: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+})
 
 // update text content of the workshop
-WorkshopSchema.methods.updateContent = async function (newContent: string): Promise<void> {
-    this.textContent = newContent;
-    await this.save();
-};
+WorkshopSchema.methods.updateContent = async function (
+  newContent: string
+): Promise<void> {
+  this.textContent = newContent
+  await this.save()
+}
 
 // mongoose model
-const Workshop = mongoose.model<IWorkshop>('Workshop', WorkshopSchema);
-export { Workshop, IWorkshop };
+const Workshop = mongoose.model<IWorkshop>("Workshop", WorkshopSchema)
+export { Workshop, IWorkshop }
