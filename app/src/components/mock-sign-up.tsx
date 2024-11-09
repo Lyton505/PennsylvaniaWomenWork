1cbc9335ccee1507f6e54c26512ec677cab83062
Lyton <lytonm@lyton.dev>
2024-11-09 17:18:50 -0600

    feat: mock frontend files for testing auth
    Added files to make it easier to test auth. Files need to be changed for a better UX

---
+++ app/src/components/mock-sign-up.tsx
@@ -0,0 +1,37 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+export const SignupButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleSignUp = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile",
+      },
+      authorizationParams: {
+        screen_hint: "signup",
+      },
+    });
+  };
+
+  return (
+    <button
+      style={{
+        margin: "0 12px",
+        padding: "8px 16px",
+        backgroundColor: "#4285f4",
+        color: "white",
+        border: "none",
+        borderRadius: "4px",
+        cursor: "pointer",
+        fontSize: "14px",
+        fontWeight: 500,
+        transition: "background-color 0.2s ease",
+      }}
+      onClick={handleSignUp}
+    >
+      Sign Up
+    </button>
+  );
+};
---
+++ app/src/pages/auth-callback.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const AuthCallback = () => {
+  return <Navbar />;
+};
+
+export default AuthCallback;
