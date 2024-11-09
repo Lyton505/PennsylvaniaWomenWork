2cc131c2b74bc3205898e2d74332b980c3424da3
Lyton <lytonm@lyton.dev>
2024-11-09 17:16:00 -0600

    feat: added frontend basic auth0 solution
    Implements full auth using auth0 on the frontend. Does not include route guards.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,18 +4,19 @@ import Home from "./pages/Home";
 import MentorDashboard from "./pages/MentorDashboard";
 import CreateWorkshop from "./pages/CreateWorkshop";
 import CreateMeeting from "./pages/CreateMeeting";
+import AuthCallback from "./pages/auth-callback";
+
 function App(): ReactElement {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/mentor" element={<MentorDashboard />} />
           <Route path="/create-workshop" element={<CreateWorkshop />} />
           <Route path="/create-meeting" element={<CreateMeeting />} />
-        </Routes>
-      </Router>
+          <Route path="/callback" element={<AuthCallback />} />
+      </Routes>
     </div>
   );
 }
