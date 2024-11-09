b8125aa31eb5bccd8ebd9701b6360cdaf0cbbcc7
Lyton <lytonm@lyton.dev>
2024-11-09 15:10:53 -0600

    ref: server.ts to include port number in console log

--- api/src/server.ts
+++ api/src/server.ts
@@ -16,4 +16,6 @@ app.use("/workshop", routes.workshop);
 
 connectDB();
 
-app.listen(process.env.PORT || 8000, () => console.log("Server running..."));
+app.listen(process.env.PORT || 8000, () =>
+  console.log(`Server running on port ${process.env.PORT || 8000}`)
+);
