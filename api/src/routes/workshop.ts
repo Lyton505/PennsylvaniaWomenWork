fb9c9de6aca003604e559bceebc91e2db070c2c5
Sophia Hooley <143217945+Sophiahooley@users.noreply.github.com>
2024-11-10 16:58:19 -0600

    actually run prettier

--- api/src/routes/workshop.ts
+++ api/src/routes/workshop.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dbConnect from '../config/db'; // Import the dbConnect function
+import express from "express";
+import mongoose from "mongoose";
+import dbConnect from "../config/db"; // Import the dbConnect function
 
-import { createWorkshop, getWorkshop } from '../controllers/workshopController';
+import { createWorkshop, getWorkshop } from "../controllers/workshopController";
 
 const router = express.Router();
 
@@ -15,14 +15,14 @@ const workshopIDSchema = new mongoose.Schema({
   s3ID: String,
 });
 
-const Workshop = mongoose.model('WorkshopID', workshopIDSchema);
+const Workshop = mongoose.model("WorkshopID", workshopIDSchema);
 
 // Route to create a new workshop
-router.post('/create-workshop', async (req: any, res: any) => {
+router.post("/create-workshop", async (req: any, res: any) => {
   const { name, s3id } = req.body;
 
   if (!name || !s3id) {
-    return res.status(400).json({ message: 'Missing required fields' });
+    return res.status(400).json({ message: "Missing required fields" });
   }
 
   // Create a new workshop
@@ -34,21 +34,21 @@ router.post('/create-workshop', async (req: any, res: any) => {
   try {
     const savedWorkshop = await newWorkshop.save();
     res.status(201).json({
-      message: 'Workshop created successfully',
+      message: "Workshop created successfully",
       WorkshopID: savedWorkshop,
     });
   } catch (error) {
-    res.status(401).json({ message: 'Failed to create workshop', error });
+    res.status(401).json({ message: "Failed to create workshop", error });
   }
 });
 
 // router.post("/workshops", createWorkshop)
 // router.get('/workshops/:id', getWorkshop);
 router.get(
-  '/workshops/:id',
+  "/workshops/:id",
   async (req: express.Request, res: express.Response) => {
     await getWorkshop(req, res);
-  },
+  }
 );
 
 // POPULATE VERSION (if details of mentor/mentee objects are needed on the frontend like name or picture)
