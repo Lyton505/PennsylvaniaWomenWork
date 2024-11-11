98da4e513ba38a2ce4c4007187681b276d22c0bf
Lyton <lytonm@lyton.dev>
2024-11-11 09:54:06 -0600

    📨 feat: implemented user invite with sendgrid (#3)
    * feat: implemented user invite with sendgrid

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 import express from "express";
 import bodyParser from "body-parser";
 import connectDB from "./config/db";
-
 import * as routes from "./routes/index";
+import path from "path";
 
 var cors = require("cors");
 
@@ -16,4 +19,6 @@ app.use("/workshop", routes.workshop);
 
 connectDB();
 
-app.listen(process.env.PORT || 8000, () => console.log("Server running..."));
+app.listen(process.env.PORT || 8000, () =>
+  console.log(`Server running on port ${process.env.PORT || 8000}`),
+);
