ee027834e786a5ed7ec71b633a141a1cd605fa91
Kiersten Brennan <kiersten.brennan@vanderbilt.edu>
2024-11-15 10:06:19 -0600

    Mentee Dashboard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { type ReactElement } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import MentorDashboard from "./pages/MentorDashboard"
+import MenteeDashboard from "./pages/MenteeDashboard"
 import CreateWorkshop from "./pages/CreateWorkshop"
 function App(): ReactElement {
   return (
@@ -11,6 +12,7 @@ function App(): ReactElement {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/mentor" element={<MentorDashboard />} />
+          <Route path="/mentee" element={<MenteeDashboard />} />
           <Route path="/create-workshop" element={<CreateWorkshop />} />
         </Routes>
       </Router>
