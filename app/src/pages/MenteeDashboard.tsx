c603f8120a1fe53d1fce0ed19fe558b63193334c
Kiersten Brennan <kiersten.brennan@vanderbilt.edu>
2024-12-06 09:46:24 -0600

    Calendar

--- app/src/pages/MenteeDashboard.tsx
+++ app/src/pages/MenteeDashboard.tsx
@@ -7,7 +7,7 @@ const MenteeDashboard = () => {
       <Navbar />
       <div className="Flex-row Justify-content--spaceBetween">
         <div className="Block Width--60 Margin-right--40 Margin-left--40 Margin-top--40 Height--100vh">
-          <div className="Block-header Text-color--gray-600 Margin-bottom--20">
+          <div className="Block-header Text-color--gray-600 Margin-bottom--20" style={{fontSize:"20px"}}>
             My courses
           </div>
 
@@ -67,10 +67,54 @@ const MenteeDashboard = () => {
             </div>
           </div>
         </div>
+
+        
         <div className="Block Width--40 Margin-right--40 Margin-top--40 Height--100vh">
-          <div className="Block-header Text-color--gray-600 Margin-bottom--20">
-            Upcoming Events
+          <div className="Block-header Text-color--gray-1000 Margin-bottom--20" style={{ borderBottom: '1px solid #e0e0e0', paddingBottom: '10px',textAlign:"center" }}>
+            Upcoming Events!
           </div>
+          <div className="Block-subtitle Margin-top--40" style={{borderBottom: '1px solid #e0e0e0', width: "50%"}}>
+            June
+          </div>
+
+          <div className="Margin-top--20">
+            <div className="Flex-row Margin-bottom--10">
+              <div className="Margin-right--40" style={{ textAlign: 'center', width: '40px' }}>
+                <div className="Text-color--gray-800">wed</div>
+                <div className="Text-fontSize--30 Text-color--gray-1000">25</div>
+              </div>
+              <div>
+                <div className="Text-fontSize--16 Text-color--gray-1000">
+                  Mock Interview Session
+                </div>
+                <div className="Text-fontSize--14 Text-color--gray-800">
+                  Practice your interview skills with an industry professional
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="Margin-top--20">
+            <div className="Flex-row Margin-bottom--10">
+              <div className="Margin-right--40" style={{ textAlign: 'center', width: '40px' }}>
+                <div className="Text-color--gray-800">wed</div>
+                <div className="Text-fontSize--30 Text-color--gray-1000">25</div>
+              </div>
+              <div>
+                <div className="Text-fontSize--16 Text-color--gray-1000">
+                  Resume Workshop
+                </div>
+                <div className="Text-fontSize--14 Text-color--gray-800">
+                  Develop your resume with a senior employee
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
+
+
+          
         </div>
       </div>
     </>
