650c21003ed8057da76f34124e41d216640531c8
Lyton <lytonm@lyton.dev>
2025-01-27 17:53:25 -0600

    feat: deafult to login page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,30 +12,39 @@ import AuthCallback from "./pages/auth-callback"
 import LoginRedirect from "./pages/LoginRedirect"
 import Logout from "./pages/Logout"
 import Profile from "./pages/Profile"
+import { useAuth0 } from "@auth0/auth0-react"
 
 function App(): ReactElement {
+  const { isAuthenticated } = useAuth0()
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<LoginRedirect />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/mentor" element={<MentorDashboard />} />
-        <Route path="/mentee" element={<MenteeDashboard />} />
-        <Route path="/confirmLogout" element={<ConfirmLogout />} />
-        <Route path="/create-workshop" element={<CreateWorkshop />} />
-        <Route path="/create-meeting" element={<CreateMeeting />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route
-          path="/mentor/mentee-information"
-          element={<MenteeInformation />}
-        />
-        <Route
-          path="/mentor/workshop-information"
-          element={<WorkshopInformation />}
-        />
         <Route path="/callback" element={<AuthCallback />} />
+        {!isAuthenticated ? (
+          <><Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<LoginRedirect />} /></>
+
+        ) : (
+          <>< Route path="/login" element={<LoginRedirect />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/mentor" element={<MentorDashboard />} />
+            <Route path="/mentee" element={<MenteeDashboard />} />
+            <Route path="/confirmLogout" element={<ConfirmLogout />} />
+            <Route path="/create-workshop" element={<CreateWorkshop />} />
+            <Route path="/create-meeting" element={<CreateMeeting />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route
+              path="/mentor/mentee-information"
+              element={<MenteeInformation />}
+            />
+            <Route
+              path="/mentor/workshop-information"
+              element={<WorkshopInformation />}
+            />
+            <Route path="/callback" element={<AuthCallback />} /></>)
+        }
       </Routes>
     </div>
   )
