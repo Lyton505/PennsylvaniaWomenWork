258938625f77afb4e2c41c0b67d5f4bac6abd211
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2025-01-28 09:29:20 -0600

    small ui changes

--- app/src/pages/CreateWorkshop.tsx
+++ app/src/pages/CreateWorkshop.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Formik, Form, Field } from "formik"
 import * as Yup from "yup"
 import Navbar from "../components/Navbar"
@@ -41,133 +41,133 @@ const CreateWorkshop = () => {
     }
   }
 
-const [isModal, setIsModal] = useState(false)
-const [isLoading, setIsLoading] = useState(false)
-const [errorMessage, setErrorMessage] = useState("")
-const [fileTitles, setFileTitles] = useState<string[]>([])
-const [fileAdded, setFileAdded] = useState(false)
-const [success, setSuccess] = useState(false)
+  const [isModal, setIsModal] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+  const [fileTitles, setFileTitles] = useState<string[]>([])
+  const [fileAdded, setFileAdded] = useState(false)
+  const [success, setSuccess] = useState(false)
 
-const fileUploadInitialValues = {
+  const fileUploadInitialValues = {
     title: "",
-    desc: "",        
+    desc: "",
     file: "",
-}
-const fileValidation = Yup.object().shape({
+  }
+  const fileValidation = Yup.object().shape({
     title: Yup.string().required("Title is required"),
     desc: Yup.string().required("Description is required"),
-    file: Yup.mixed().required("Please select a file")
-})
+    file: Yup.mixed().required("Please select a file"),
+  })
 
-const handleFileSumbit = async (values: any, { resetForm }: any) => {
+  const handleFileSumbit = async (values: any, { resetForm }: any) => {
     setIsLoading(true)
     setErrorMessage("")
 
     try {
-        const finalData = {
-            title: values.title,
-            desc: values.desc,
-            file: values.file,
-        }
-        setFileTitles((prevTitles) => [...prevTitles, values.title]);
-        setFileAdded(true)
-        console.log("Submitting data:", finalData)
-        setSuccess(true)
-        setErrorMessage("")
-        resetForm()
+      const finalData = {
+        title: values.title,
+        desc: values.desc,
+        file: values.file,
+      }
+      setFileTitles((prevTitles) => [...prevTitles, values.title])
+      setFileAdded(true)
+      console.log("Submitting data:", finalData)
+      setSuccess(true)
+      setErrorMessage("")
+      resetForm()
     } catch (error) {
-        console.error("Error submitting:", error)
+      console.error("Error submitting:", error)
     } finally {
-        setIsLoading(false)
+      setIsLoading(false)
     }
-}
+  }
 
   return (
     <>
-    {isModal && (
-            <Modal
-            header="Add New Files"
-            subheader="Select Files to Upload"
-            action={() => setIsModal(false)}
-            body={
-                <Formik
-                initialValues={fileUploadInitialValues}
-                validationSchema={fileValidation}
-                onSubmit={handleFileSumbit}
-                >
-                {({ values, errors, touched, isSubmitting }) => (
-                    <Form>
-                    <div className="Form-group">
-                        <label htmlFor="title">Title</label>
-                        <Field
-                        className="Form-input-box"
-                        type="text"
-                        id="title"
-                        name="title"
-                        />
-                        {errors.title && touched.title && (
-                        <div className="Form-error">{errors.title}</div>
-                        )}
-                    </div>
-                    <div className="Form-group">
-                        <label htmlFor="desc">Description</label>
-                        <Field
-                        as="textarea"
-                        className="Form-input-box text-area"
-                        type="text"
-                        id="desc"
-                        name="desc"
-                        rows="4"
-                        />
-                        {errors.desc && touched.desc && (
-                        <div className="Form-error">{errors.desc}</div>
-                        )}
-                    </div>
-                    <div className="Form-group">
-                        <label htmlFor="file">Files</label>
-                        <Field
-                        className="Form-input-box"
-                        type="file"
-                        id="file"
-                        name="file"
-                        />
-                        {errors.file && touched.file && (
-                        <div className="Form-error">{errors.file}</div>
-                        )}
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="Button Margin-top--10 Button-color--teal-1000 Width--100"
-                        disabled={
-                        Object.keys(errors).length > 0 ||
-                        !Object.keys(touched).length ||
-                        isSubmitting
-                        }
-                        // Disable button if there are errors or no fields are touched or form is submitting
-                    >
-                        {isSubmitting ? (
-                        <AsyncSubmit loading={isLoading} />
-                        ) : (
-                        "Upload Files"
-                        )}
-                    </button>
-
-                    {errorMessage && (
-                        <div className="Form-error">{errorMessage}</div>
+      {isModal && (
+        <Modal
+          header="Add New Files"
+          subheader="Select Files to Upload"
+          action={() => setIsModal(false)}
+          body={
+            <Formik
+              initialValues={fileUploadInitialValues}
+              validationSchema={fileValidation}
+              onSubmit={handleFileSumbit}
+            >
+              {({ values, errors, touched, isSubmitting }) => (
+                <Form>
+                  <div className="Form-group">
+                    <label htmlFor="title">Title</label>
+                    <Field
+                      className="Form-input-box"
+                      type="text"
+                      id="title"
+                      name="title"
+                    />
+                    {errors.title && touched.title && (
+                      <div className="Form-error">{errors.title}</div>
                     )}
-
-                    {success && (
-                        <div className="Form-success">
-                        File uploaded successfully!
-                        </div>
+                  </div>
+                  <div className="Form-group">
+                    <label htmlFor="desc">Description</label>
+                    <Field
+                      as="textarea"
+                      className="Form-input-box text-area"
+                      type="text"
+                      id="desc"
+                      name="desc"
+                      rows="4"
+                    />
+                    {errors.desc && touched.desc && (
+                      <div className="Form-error">{errors.desc}</div>
                     )}
-                    </Form>
-                )}
-                </Formik>
-            }
-            />
-        )}
+                  </div>
+                  <div className="Form-group">
+                    <label htmlFor="file">Files</label>
+                    <Field
+                      className="Form-input-box"
+                      type="file"
+                      id="file"
+                      name="file"
+                    />
+                    {errors.file && touched.file && (
+                      <div className="Form-error">{errors.file}</div>
+                    )}
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="Button Margin-top--10 Button-color--teal-1000 Width--100"
+                    disabled={
+                      Object.keys(errors).length > 0 ||
+                      !Object.keys(touched).length ||
+                      isSubmitting
+                    }
+                    // Disable button if there are errors or no fields are touched or form is submitting
+                  >
+                    {isSubmitting ? (
+                      <AsyncSubmit loading={isLoading} />
+                    ) : (
+                      "Upload Files"
+                    )}
+                  </button>
+
+                  {errorMessage && (
+                    <div className="Form-error">{errorMessage}</div>
+                  )}
+
+                  {success && (
+                    <div className="Form-success">
+                      File uploaded successfully!
+                    </div>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          }
+        />
+      )}
       <Navbar />
 
       <div className="Block Width--70 Margin-right--80 Margin-left--80 Margin-top--40">
@@ -223,31 +223,36 @@ const handleFileSumbit = async (values: any, { resetForm }: any) => {
                     )}
                   </div>
                 </div>
-                { fileAdded && (<div>
-                  <div className="Flex-row Text-fontSize--16 Text-color--gray-1000 Margin-bottom--8">
-                    <div className="description">Files:</div>
-                  </div>
-                  <ul>
-                    {fileTitles.map((title, index) => (
-                    <li key={index}>{title}</li>
-                    ))}
-                  </ul>
+                {fileAdded && (
+                  <div>
+                    <div className="Flex-row Text-fontSize--16 Margin-bottom--8">
+                      <div className="description">Files:</div>
+                    </div>
+                    <ul>
+                      {fileTitles.map((title, index) => (
+                        <li key={index}>{title}</li>
+                      ))}
+                    </ul>
                   </div>
                 )}
-                <div className="Flex-row Justify-content--center">
+                <div className="Flex-row Justify-content--center Margin-top--30">
                   <button
                     type="button"
-                    className="Button Margin-top--30 Button-color--teal-1000 Width--50 Margin-right--10"
+                    className="Button Button-color--teal-1000 Width--50 Margin-right--10"
                     onClick={() => setIsModal(true)}
                   >
                     Add Files
                   </button>
                   <button
                     type="submit"
-                    className="Button Margin-top--30 Button-color--teal-1000 Width--50"
+                    className="Button Button-color--teal-1000 Width--50 Button--hollow"
                     disabled={isSubmitting} // Disable button while form is submitting
                   >
-                    {isSubmitting ? "Submitting..." : "Create Workshop"}
+                    {isSubmitting ? (
+                      <AsyncSubmit loading={isSubmitting} />
+                    ) : (
+                      "Create Workshop"
+                    )}
                   </button>
                 </div>
               </Form>
