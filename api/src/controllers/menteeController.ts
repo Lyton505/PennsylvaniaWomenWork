4878f9e287a9af0dd315493fd6ff48ad54e9e9a5
Samuel Thompson <samuelthomp14@gmail.com>
2025-02-16 13:44:41 -0600

    create addWorkshopToMentee endpoint

--- api/src/controllers/menteeController.ts
+++ api/src/controllers/menteeController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Workshop } from "../model/Workshop";
+import { Mentee } from "../model/Mentee";
 
 export const getWorkshopsForMentee = async (req: Request, res: Response) => {
   try {
@@ -11,4 +12,28 @@ export const getWorkshopsForMentee = async (req: Request, res: Response) => {
       .status(500)
       .json({ message: "Error retrieving workshops for mentee", error });
   }
+};
+
+export const addWorkshopToMentee = async (req: Request, res: Response) => {
+  const { menteeId } = req.params;
+  const { workshopId } = req.body;
+
+  if (!menteeId || !workshopId) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const updatedMentee = await Mentee.findByIdAndUpdate(
+        menteeId,
+        { $push: { workshops: workshopId } },  
+        { new: true, safe: true, upsert: false }
+    );
+
+    if (!updatedMentee) {
+        return res.status(404).send('Mentee not found');
+    }
+    res.json(updatedMentee);
+} catch (error) {
+    res.status(500).send((error as Error).toString());
+}
 };
