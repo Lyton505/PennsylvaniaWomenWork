b130cb0454e33ea1800fb31af95b3d904bb3064f
Samuel Thompson <samuelthomp14@gmail.com>
2025-02-23 15:36:56 -0600

    add endpoint to get S3 read url

--- api/src/routes/resource.ts
+++ api/src/routes/resource.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createResource,
   getResourcesByWorkshopId,
+  generateRetrievalURL,
 } from "../controllers/resourceController";
 
 const router = express.Router();
@@ -11,5 +12,7 @@ router.post("/create-resource", createResource);
 
 // Route to get resources by workshop ID -- not wired
 router.get("/workshop/:workshopId/resources", getResourcesByWorkshopId);
+
+router.get("/getURL/:objectId", generateRetrievalURL);
 
 export default router;
