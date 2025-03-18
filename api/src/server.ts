fde559ad9564f9ddc2cd41ac11382fb17ae9d787
Lyton <lytonm@lyton.dev>
2025-03-18 17:21:34 -0400

    fix: reference client origin variable

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,9 +12,7 @@ import router from "./routes";
 var cors = require("cors");
 
 const app = express();
-app.use(
-  cors({ origin: "https://pennsylvaniawomenwork-staging.up.railway.app" })
-);
+app.use(cors({ origin: process.env.CLIENT_ORIGIN_URL }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
