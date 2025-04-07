a1659a86142c7a8e34a762bff07f43fb4b72ae01
charlotteconze <85513920+charlotteconze@users.noreply.github.com>
2025-04-06 20:52:48 -0500

    Update BoardDashboard.tsx

--- app/src/pages/BoardDashboard.tsx
+++ app/src/pages/BoardDashboard.tsx
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "../components/Navbar";
-import Modal from "../components/Modal";
-import { useNavigate } from "react-router-dom";
-import { api } from "../api";
-import Event, { EventData } from "../components/Event";
-import { useUser } from "../contexts/UserContext";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState, useEffect } from "react"
+import Navbar from "../components/Navbar"
+import Modal from "../components/Modal"
+import { useNavigate } from "react-router-dom"
+import { api } from "../api"
+import Event, { EventData } from "../components/Event"
+import { useUser } from "../contexts/UserContext"
+import { useAuth0 } from "@auth0/auth0-react"
 
 interface File {
-  name: string;
-  description: string;
-  s3id: string;
-  tags: [string];
+  _id: string
+  name: string
+  description: string
+  tags: string[]
+  s3id: string
 }
 
 const BoardDashboard = () => {
-  const navigate = useNavigate();
-  const [events, setEvents] = useState<EventData[]>([]);
-  const [files, setFiles] = useState<File[]>([]);
-  const [selectedEvent, setSelectedEvent] = useState<EventData | null>(null);
-  const { user } = useUser();
-  const userId = user?._id;
-  const [loading, setLoading] = useState(true);
-  const start = selectedEvent ? new Date(selectedEvent.startTime) : null;
-  const end = selectedEvent ? new Date(selectedEvent.endTime) : null;
-  const eventDate = selectedEvent ? new Date(selectedEvent.date) : null;
+  const navigate = useNavigate()
+  const [events, setEvents] = useState<EventData[]>([])
+  const [files, setFiles] = useState<File[]>([])
+  const [selectedEvent, setSelectedEvent] = useState<EventData | null>(null)
+  const { user } = useUser()
+  const userId = user?._id
+  const [loading, setLoading] = useState(true)
+  const start = selectedEvent ? new Date(selectedEvent.startTime) : null
+  const end = selectedEvent ? new Date(selectedEvent.endTime) : null
+  const eventDate = selectedEvent ? new Date(selectedEvent.date) : null
 
   const formattedSubheader =
     eventDate && start && end
@@ -36,31 +37,31 @@ const BoardDashboard = () => {
             hour: "2-digit",
             minute: "2-digit",
             hour12: true,
-          },
+          }
         )} - ${end.toLocaleTimeString("en-US", {
           hour: "2-digit",
           minute: "2-digit",
           hour12: true,
         })}`
-      : "";
+      : ""
 
   useEffect(() => {
-    if (!userId) return;
+    if (!userId) return
 
     const fetchData = async () => {
       try {
         const [eventsResponse, filesResponse] = await Promise.all([
           api.get(`/api/event/${userId}`),
           api.get(`/api/boardFile/get-board-files`),
-        ]);
+        ])
 
         setFiles(
-            filesResponse.data.map((file: any) => ({
-                name: file.name,
-                description: file.description,
-                s3id: file.s3id,
-                tags: file.tags || [],
-            })),
+          filesResponse.data.map((file: any) => ({
+            name: file.name,
+            description: file.description,
+            s3id: file.s3id,
+            tags: file.tags || [],
+          }))
         )
 
         setEvents(
@@ -72,59 +73,59 @@ const BoardDashboard = () => {
             date: event.date,
             userIds: event.users || [],
             calendarLink: event.calendarLink || "",
-          })),
-        );
+          }))
+        )
 
-        setEvents(eventsResponse.data);
+        setEvents(eventsResponse.data)
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching data:", error)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
+    }
 
-    fetchData();
-  }, [userId]);
+    fetchData()
+  }, [userId])
 
-  const handleWorkshopClick = (workshopId: string) => {
+  const handleFileClick = (workshopId: string) => {
     navigate(`/volunteer/workshop-information`, {
       state: { workshopId },
-    });
-  };
+    })
+  }
 
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
 
   const eventsByMonth: { [key: string]: EventData[] } = events
     .filter((event) => new Date(event.date) >= today)
     .sort(
       (a, b) =>
-        new Date(a.startTime).getTime() - new Date(b.startTime).getTime(),
+        new Date(a.startTime).getTime() - new Date(b.startTime).getTime()
     ) // Sort events chronologically
     .reduce(
       (acc, event) => {
-        const eventDate = new Date(event.startTime);
-        const month = eventDate.toLocaleString("default", { month: "long" });
+        const eventDate = new Date(event.startTime)
+        const month = eventDate.toLocaleString("default", { month: "long" })
 
         if (!acc[month]) {
-          acc[month] = [];
+          acc[month] = []
         }
         acc[month].push({
           ...event,
           formattedDate: eventDate.toDateString(),
-        });
+        })
 
-        return acc;
+        return acc
       },
-      {} as { [key: string]: EventData[] },
-    );
+      {} as { [key: string]: EventData[] }
+    )
 
   const handleEventClick = (event: EventData) => {
-    setSelectedEvent(event);
-  };
+    setSelectedEvent(event)
+  }
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   return (
@@ -152,61 +153,63 @@ const BoardDashboard = () => {
         />
       )}
       <div className="container py-4">
-      <div className="row g-3">
-        <div className="col-lg-8">
-          <div className="Block">
-            <div className="Block-header">All Files</div>
-            <div className="Block-subtitle">
-              Select a file to access materials.
-            </div>
-            <div className="row gx-3 gy-3">
-              {files.map((item) => (
-                <div className="col-lg-4" key={item.s3id}>
+        <div className="row g-3">
+          <div className="col-lg-8">
+            <div className="Block">
+              <div className="Block-header">All Files</div>
+              <div className="Block-subtitle">
+                Select a file to access materials.
+              </div>
+              <div className="row gx-3 gy-3">
+                {files.map((item) => (
                   <div
-                    className="Mentor--card"
+                    className="col-lg-4"
+                    key={item._id}
+                    onClick={() => handleFileClick(item._id)}
                   >
-                    <div className="Mentor--card-color Background-color--teal-1000"> 
-                    <div className = "File-tags-container">  
-                    <div className = "File-tags">
-                        {item.tags.map((tag, index) => (
-                            <span key={index} className="Tag">
-                            {tag}
-                            </span>
-                        ))}
-                    </div>
-                    </div>
-                    </div>
-        
-                    <div className="Padding--10">
-                    <div className="Mentor--card-name">{item.name}</div>
-                          <div className="Mentor--card-description">
-                            {item.description}
+                    <div className="Mentor--card">
+                      <div className="Mentor--card-color Background-color--teal-1000">
+                        <div className="File-tags-container">
+                          <div className="File-tags">
+                            {item.tags.map((tag, index) => (
+                              <span key={index} className="Tag">
+                                {tag}
+                              </span>
+                            ))}
                           </div>
+                        </div>
+                      </div>
+
+                      <div className="Padding--10">
+                        <div className="Mentor--card-name">{item.name}</div>
+                        <div className="Mentor--card-description">
+                          {item.description}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="Block p-3">
+              <div className="Block-header">Upcoming Events</div>
+              <div className="Block-subtitle">Select an event to register.</div>
+              {Object.entries(eventsByMonth).map(([month, monthEvents]) => (
+                <Event
+                  key={month}
+                  month={month}
+                  events={monthEvents}
+                  onEventClick={handleEventClick}
+                />
               ))}
             </div>
           </div>
         </div>
-        <div className="col-lg-4">
-          <div className="Block p-3">
-            <div className="Block-header">Upcoming Events</div>
-            <div className="Block-subtitle">Select an event to register.</div>
-            {Object.entries(eventsByMonth).map(([month, monthEvents]) => (
-              <Event
-                key={month}
-                month={month}
-                events={monthEvents}
-                onEventClick={handleEventClick}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default BoardDashboard;
+export default BoardDashboard
